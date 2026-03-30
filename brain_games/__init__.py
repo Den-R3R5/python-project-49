40fa1ca8644e27ea4b52fec0bfa22351d2dcec0a
Den-R3R5 <deniskostin235@gmail.com>
2026-03-30 22:01:40 +1100

    update brain_games/__init__.py

--- brain_games/__init__.py
+++ brain_games/__init__.py
@@ -1,13 +1,27 @@
 from .cli import welcome_user
 from .greet import greet
-import brain_games.game as game
-from .game import hello_user
-from .game import even_game
+import brain_games.engine as engine
+from .engine import hello_user
+from .engine import question
+from .engine import user_answer
+from .engine import check_answer
+from .engine import congratulations
+from .games.even_game import even_game_rules
+from .games.even_game import even_game
+from .games.calc_game import calc_game_rules
+from .games.calc_game import calc_game
 
 __all__ = (
     "welcome_user",
     "greet",
-    "game",
+    "engine",
     "hello_user",
+    "question",
+    "user_answer",
+    "check_answer",
+    "congratulations",
+    "even_game_rules",
     "even_game",
+    "calc_game_rules",
+    "calc_game",
 )
