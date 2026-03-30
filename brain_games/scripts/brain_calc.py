aace24754380b6fdb0b8969a7f404776def81da3
Den-R3R5 <deniskostin235@gmail.com>
2026-03-30 22:07:04 +1100

    add calc game entry point

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,17 @@
+from brain_games import calc_game_rules, calc_game, hello_user, question, user_answer, check_answer, congratulations
+
+
+def main():
+    name = hello_user()
+    calc_game_rules()
+    for i in range(3):
+        game_input, game_answer = calc_game()
+        question(game_input)
+        user_input = user_answer()
+        is_user_right = check_answer(user_input, game_answer, name)
+        if is_user_right == False:
+            return
+    congratulations(name)
+
+if __name__ == "__main__":
+    main()
