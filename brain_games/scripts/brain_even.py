2ea3d3bfa6c6f286662733899f36d4f3e87ebf92
Den-R3R5 <deniskostin235@gmail.com>
2026-03-30 22:07:57 +1100

    recode

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,17 @@
-from brain_games import game, hello_user, even_game
+from brain_games import even_game_rules, even_game, hello_user, question, user_answer, check_answer, congratulations
 
 
 def main():
-    name = game.hello_user()
-    even_game(name)
+    name = hello_user()
+    even_game_rules()
+    for i in range(3):
+        game_input, game_answer = even_game()
+        question(game_input)
+        user_input = user_answer()
+        is_user_right = check_answer(user_input, game_answer, name)
+        if is_user_right == False:
+            return
+    congratulations(name)
 
 if __name__ == "__main__":
     main()
